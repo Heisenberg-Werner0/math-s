e10d25b8147185eb35d6d5f46341253394c49268
Heisenberg-Werner0 <1526890090@qq.com>
2023-12-15 16:57:48 +0800

    add reduction function in example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -1,3 +1,8 @@
-int main(){
+#include <iostream>
+#include "../include/fraction.hpp"
 
+int main() {
+    math_s::Fraction frac(5, -10);
+    frac.reduction();
+    std::cout << frac.getNumerator() << ", " << frac.getDenominator();
 }

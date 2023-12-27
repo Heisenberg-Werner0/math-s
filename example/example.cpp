813bd548a98a1e8bb189c8b41da68a1d8cb8633b
Heisenberg-Werner0 <1526890090@qq.com>
2023-12-27 17:46:29 +0800

    delete mode-group & integer

--- example/example.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../include/fraction.hpp"
-
-int main() {
-    math_s::Fraction frac(5, -10);
-    frac.reduction();
-    std::cout << frac.getNumerator() << ", " << frac.getDenominator();
-}
